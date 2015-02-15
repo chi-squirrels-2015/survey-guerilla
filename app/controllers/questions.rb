3ccf137b5a0d23f850bd2481ea5085f4d6eb8986
John N. Mandalakas & Matthew DeCarlo <pair+jnmandal.matthewwho@devbootcamp.com>
2015-02-14 18:20:13 -0600

    end of pairng day

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,6 +1,17 @@
 post '/surveys/:survey_id/questions' do
-  question = Question.create(content: params[:question][:content], survey_id: params[:survey_id])
+  puts params
+
+  survey = Survey.find(params[:survey_id])
+  question = survey.questions[(params[:question_id].to_i - 1)]
+
+  if question 
+    question.update(content: params[:question][:content], survey_id: params[:survey_id])
+  else
+    question = Question.create(content: params[:question][:content], survey_id: params[:survey_id])
+  end
   
+  question.answers.delete_all
+
   params[:question][:answers].each do |content|
     Answer.create(question_id: question.id, content: content)
   end
